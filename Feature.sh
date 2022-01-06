e52348d0abfc863c7b33cef0758aab618251b680
JustinFrost47 <rinojonathanarul@gmail.com>
2022-01-07 00:35:24 +0530

    New feature with working second part

--- Feature.sh
+++ Feature.sh
@@ -1,1 +1,25 @@
- 
+ #/usr/bin/bash
+
+ hour=1
+ minute=2
+ second=3
+
+ while [[ $hour>0 ]]
+ do
+    echo "$hour"
+
+    while [[ $minute>0 ]]
+    do
+        echo "$minute"
+
+        while [[ $second>0 ]]
+            do
+                echo "$hour : $minute : $second"
+                second=$(( $second-1 ))
+                sleep 1s
+
+            done
+        minute=$(($minute-1))
+        second=60
+    done
+done
