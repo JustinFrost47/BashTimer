2c55333640a6d756974c7fc2a9f5e7539ab883aa
JustinFrost47 <rinojonathanarul@gmail.com>
2022-01-07 01:34:44 +0530

    New feature - functional countdown

--- Feature.sh
+++ Feature.sh
@@ -4,20 +4,30 @@
  minute=1
  second=3
 
+#This is a developmental feature and will be integrated to the main timer
+#Finally, the timer now shows count
 
  while [[ $hour -ge 0 ]]
  do
     while [[ $minute -ge 0 ]]
     do
+
         while [[ $second -ge 0 ]]
             do
-                echo "$hour h : $minute m : $second s"
+
+                clear
+                echo " $hour h : $minute m : $second s"
                 second=$(( $second-1 ))
                 sleep 1s
+
             done
+
         minute=$(($minute-1))
-        second=5
+        second=60
+
     done
+
     hour=$(( $hour-1 ))
-    minute=5
+    minute=60
+
 done
